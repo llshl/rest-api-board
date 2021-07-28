f8f0433591a735c562565fb601418b2f9614dc80
llshl <lsh970708@naver.com>
2021-07-28 22:52:37 +0900

    add: index controller 추가

--- src/main/java/com/example/restapiboard/controller/HomeController.java
+++ src/main/java/com/example/restapiboard/controller/HomeController.java
@@ -1,45 +1,24 @@
 package com.example.restapiboard.controller;
 
-import com.example.restapiboard.dto.MemberDto;
-import com.example.restapiboard.service.MemberService;
+import com.example.restapiboard.security.MemberDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.servlet.http.HttpSession;
+import java.security.Principal;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final MemberService memberService;
-
     @GetMapping("/")
-    public String home(){
+    public String home(Model model, @AuthenticationPrincipal MemberDetailsImpl memberDetails, Principal principal){
+        model.addAttribute("username", memberDetails.getUsername());
+        System.out.println("멤버 id 반환 되나? "+memberDetails.getMemberVo().getMember_id());
         return "index";
-    }
-
-    //아이디 중복체크 ajax 구현
-    //nickname 중복체크 ajax 구현
-    @PostMapping("/login")
-    public String login(@RequestBody MemberDto memberDto, HttpSession session, Model model){
-        log.info("로그인 시도");
-        String result = memberService.login(memberDto,session);
-        if(result.equals("success")){
-            //성공
-            log.info("로그인 성공");
-            return "redirect:/";
-        }
-        else if(result.equals("wrong-password")){
-            //비밀번호만 틀림
-            return "redirect:/";
-        }
-        //아이디가 없음
-        return "redirect:/";
     }
 }
