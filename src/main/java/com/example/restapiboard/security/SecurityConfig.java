ac6d9e5443fa04a5e46470c47a96b152baa43850
llshl <lsh970708@naver.com>
2021-07-28 22:53:28 +0900

    add: 스프링 시큐리티 설정 클래스 추가

---
+++ src/main/java/com/example/restapiboard/security/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.example.restapiboard.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        http.authorizeRequests()
+                // image 폴더를 login 없이 허용
+                .antMatchers("/images/**").permitAll()  //image로 시작하는 모든 부분들은 인증없이 접근가능
+                // css 폴더를 login 없이 허용
+                .antMatchers("/css/**").permitAll()     //css로 시작하는 모든 부분들은 인증없이 접근가능
+                .antMatchers("/user/**").permitAll()    //user로 시작하는 모든 부분들은 인증없이 접근가능
+                .antMatchers("/h2-console/**").permitAll()    //db도 인증없이 접근가능
+                .anyRequest().authenticated()   //모든 요청이 인증이 필요함(css 파일도 인증이 필요함)
+
+                .and()
+                .formLogin()            //로그인페이지는 인증 필요 없음
+                .loginPage("/user/login")   //로그인 페이지 지정
+                .failureUrl("/user/login/error")       //로그인 실패 시 어디로 갈지 지정
+                .defaultSuccessUrl("/") //로그인 완료 시 이동할 페이지 지정
+                .permitAll()
+
+                .and()
+                .logout()
+                .logoutUrl("/user/logout")  //로그아웃 컨트롤러를 따로 만들 필요 없이 이 url요청이 들어오면 로그아웃 해준다
+                .permitAll()
+
+                .and()
+                .exceptionHandling()    //인가가 안된 사용자일 경우
+                .accessDeniedPage("/user/forbidden");   //포비든 페이지로 이동
+
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
