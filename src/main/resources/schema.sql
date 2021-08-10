8c989063638962cc89688c846a1ea7372ab0d1cd
llshl <lsh970708@naver.com>
2021-08-11 03:57:14 +0900

    update: LIKE 테이블에서 parent_type 필드 제거

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -22,6 +22,7 @@ CREATE TABLE COMMENT(
     comment_id INT AUTO_INCREMENT,
     board_id INT NOT NULL,
     member_id INT NOT NULL,
+    member_nickname VARCHAR(500) NOT NULL ,
     content VARCHAR(500) NOT NULL,
     date DATETIME NOT NULL,
     isUpdated BOOLEAN NOT NULL,
@@ -31,9 +32,6 @@ CREATE TABLE _LIKE(
     like_id INT AUTO_INCREMENT,
     parent_id INT NOT NULL,
     member_id INT NOT NULL,
---     parent_type ENUM('board','comment') NOT NULL,
---     like_type ENUM('like','dislike') NOT NULL,
-    parent_type BOOLEAN NOT NULL,
     like_type BOOLEAN NOT NULL,
     PRIMARY KEY (like_id)
 );
